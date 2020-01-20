691c14b0c8eae8f2b3802ffa6700fbb150bac638
Anton <asharikov@icecompany.org>
2020-01-20 17:04:23 +0300

    Another symbols

--- administrator/components/com_companies/models/rules/varchar.php
+++ administrator/components/com_companies/models/rules/varchar.php
@@ -4,5 +4,5 @@ defined('_JEXEC') or die;
 
 class JFormRuleVarchar extends FormRule
 {
-    protected $regex = '^([А-Яа-яA-Za-z-]{1,254})$';
+    protected $regex = '^([А-Яа-яA-Za-z\+\.\s-]{1,254})$';
 }
