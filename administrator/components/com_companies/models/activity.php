0d84544869beea168eb257cf5bdc7ff63759fb94
Anton <asharikov@icecompany.org>
2020-01-30 12:10:25 +0300

    CRM-14 Activity model

---
+++ administrator/components/com_companies/models/activity.php
@@ -0,0 +1,74 @@
+<?php
+defined('_JEXEC') or die;
+use Joomla\CMS\MVC\Model\AdminModel;
+
+class CompaniesModelActivity extends AdminModel {
+
+    public function getItem($pk = null)
+    {
+        return parent::getItem($pk);
+    }
+
+    public function save($data)
+    {
+        return parent::save($data);
+    }
+
+    public function getTable($name = 'Activities', $prefix = 'TableCompanies', $options = array())
+    {
+        return JTable::getInstance($name, $prefix, $options);
+    }
+
+    public function getForm($data = array(), $loadData = true)
+    {
+        $form = $this->loadForm(
+            $this->option.'.activity', 'activity', array('control' => 'jform', 'load_data' => $loadData)
+        );
+        if (empty($form))
+        {
+            return false;
+        }
+
+        return $form;
+    }
+
+    protected function loadFormData()
+    {
+        $data = JFactory::getApplication()->getUserState($this->option.'.edit.activity.data', array());
+        if (empty($data))
+        {
+            $data = $this->getItem();
+        }
+
+        return $data;
+    }
+
+    protected function prepareTable($table)
+    {
+        $nulls = array(); //Поля, которые NULL
+        foreach ($nulls as $field)
+        {
+            if (!strlen($table->$field)) $table->$field = NULL;
+        }
+        parent::prepareTable($table);
+    }
+
+    protected function canEditState($record)
+    {
+        $user = JFactory::getUser();
+
+        if (!empty($record->id))
+        {
+            return $user->authorise('core.edit.state', $this->option . '.activity.' . (int) $record->id);
+        }
+        else
+        {
+            return parent::canEditState($record);
+        }
+    }
+
+    public function getScript()
+    {
+        return 'administrator/components/' . $this->option . '/models/forms/activity.js';
+    }
+}
