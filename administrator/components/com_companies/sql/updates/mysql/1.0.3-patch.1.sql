4d26aea66916a6ad65b223f1da20b0ea8a7f2b38
Anton <asharikov@icecompany.org>
2020-02-20 15:42:53 +0300

    CRM-41 Размер поля для площади в досье

--- administrator/components/com_companies/sql/updates/mysql/1.0.3-patch.1.sql
+++ administrator/components/com_companies/sql/updates/mysql/1.0.3-patch.1.sql
@@ -1,2 +1,3 @@
 alter table `#__mkv_companies_army_history`
-    add is_report boolean null default null comment 'Участвует с докладом, а не своё мероприятие' after is_ndp;
+    add is_report boolean null default null comment 'Участвует с докладом, а не своё мероприятие' after is_ndp,
+    modify square_value double(6,2) null default null;
