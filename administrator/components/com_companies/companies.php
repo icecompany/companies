d6bdb512fcc7d5ebde2092327a544c86cbee1a9f
Anton <asharikov@icecompany.org>
2020-06-15 09:38:21 +0300

    Require Finances helper

--- administrator/components/com_companies/companies.php
+++ administrator/components/com_companies/companies.php
@@ -25,6 +25,7 @@ JFactory::getLanguage()->load('com_mkv', JPATH_ADMINISTRATOR . "/components/com_
 require_once JPATH_ADMINISTRATOR . "/components/com_mkv/helpers/mkv.php";
 require_once JPATH_ADMINISTRATOR . "/components/com_prj/helpers/prj.php";
 require_once JPATH_ADMINISTRATOR . "/components/com_contracts/helpers/contracts.php";
+require_once JPATH_ADMINISTRATOR . "/components/com_finances/helpers/finances.php";
 require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/companies.php';
 require_once JPATH_COMPONENT_ADMINISTRATOR . '/passwd.php';
 $db = JFactory::getDbo();
